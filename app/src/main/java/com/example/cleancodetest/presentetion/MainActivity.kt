291f9ffd3cdf48b8adb20d3904d6931f3926143c
Nodar Belashvili <nodarbelashvili@gmail.com>
2022-12-07 16:38:17 +0400

    MVVM Patern

--- app/src/main/java/com/example/cleancodetest/presentetion/MainActivity.kt
+++ app/src/main/java/com/example/cleancodetest/presentetion/MainActivity.kt
@@ -1,38 +1,43 @@
 package com.example.cleancodetest.presentetion
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.cleanarchitectureapp.databinding.ActivityMainBinding
 import com.example.cleancodetest.data.repository.UserRepositoryImpl
 import com.example.cleancodetest.data.storage.sharedprefs.SharedPrefUserStorage
 import com.example.cleancodetest.domain.models.SaveUserNameParam
 import com.example.cleancodetest.domain.usecase.GetUserNameUseCase
 import com.example.cleancodetest.domain.usecase.SaveUserNameUseCase
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val userRepository by lazy(LazyThreadSafetyMode.NONE) { UserRepositoryImpl(userStorage = SharedPrefUserStorage(context = applicationContext)) }
-    private val getUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) { GetUserNameUseCase(userRepository = userRepository) }
-    private val saveUserNameUseCase by lazy(LazyThreadSafetyMode.NONE) { SaveUserNameUseCase(userRepository = userRepository) }
+
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.sendBtn.setOnClickListener {
-            val text = binding.dataEt.text.toString()
-            val param = SaveUserNameParam(name = text)
-            val result = saveUserNameUseCase.execute(param = param)
-            binding.dataTv.text = "Save result = $result"
+        Log.e("AAA", "Activity Created", )
+        viewModel = ViewModelProvider(this, MainViewModelFactory(this))[MainViewModel::class.java]
+        viewModel.resultLive.observe(this) {
+            binding.dataTv.text = it
         }
 
-        //
+        binding.saveBtn.setOnClickListener {
+            val text = binding.dataEt.text.toString()
+            viewModel.save(text)
+
+        }
+
         binding.receiveBtn.setOnClickListener {
-            val userName = getUserNameUseCase.execute()
-            binding.dataTv.text = "${userName.firstName}"
+            viewModel.load()
         }
     }
 }
---
+++ app/src/main/java/com/example/cleancodetest/presentetion/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.cleancodetest.presentetion
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.cleancodetest.domain.models.SaveUserNameParam
+import com.example.cleancodetest.domain.usecase.GetUserNameUseCase
+import com.example.cleancodetest.domain.usecase.SaveUserNameUseCase
+
+class MainViewModel(
+    private val getUserNameUseCase: GetUserNameUseCase,
+    private val saveUserNameUseCase: SaveUserNameUseCase
+) : ViewModel() {
+
+    var resultLive = MutableLiveData<String>()
+
+    fun save(text: String) {
+        val param = SaveUserNameParam(name = text)
+        val result = saveUserNameUseCase.execute(param = param)
+        resultLive.value = "Save result = $result"
+    }
+
+    fun load() {
+        val userName = getUserNameUseCase.execute()
+        resultLive.value = "${userName.firstName}"
+    }
+
+    init {
+        Log.e("AAA", "VM Created")
+    }
+
+    override fun onCleared() {
+        Log.e("AAA", "VM Created")
+        super.onCleared()
+    }
+}
